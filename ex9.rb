88669764388632f0929432f397bc4939f10da698
dariodaprile <dario.daprile@gmail.com>
2013-05-20 14:27:02 +0100

    exercise 9

---
+++ ex9.rb
@@ -0,0 +1,9 @@
+days = "Mon Tue Wed thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months:", months
+
+puts <<PARAGRAPH
+This is some text as in the string but it's not a string
+PARAGRAPH
