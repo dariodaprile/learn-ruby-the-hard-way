972b64c74554124acd9bb7b7f4e21d6d3dc5b74b
dariodaprile <dario.daprile@gmail.com>
2013-05-21 17:12:30 +0100

    end of exercise 22

--- ex21.rb
+++ ex21.rb
@@ -1,6 +1,6 @@
 def add(a, b)
   puts "ADDING #{a} +#{b}"
-  a + b
+  a.+(b)
 end
 
 def subtract(a, b)
@@ -10,9 +10,11 @@ end
 
 def multiply(a, b)
   puts "MULTIPLYING #{a} *#{b}"
+  a * b
 end
 def divide(a, b)
   puts "DIVIDING #{a} /#{b}"
+  a / b
 end
 
 puts "Let's do some math with just fuctions!"
@@ -23,3 +25,9 @@ weight = multiply(90, 2)
 iq = divide(100, 2)
 
 puts "Age:#{age}, Height:#{height}, Weight:#{weight}, IQ: #{iq}"
+
+puts "Here is a puzzle"
+
+what = add(age, subtract(height, multiply(weight,divide(iq,2))))
+
+puts "That becomes: #{what} Can you do it by hand?"
