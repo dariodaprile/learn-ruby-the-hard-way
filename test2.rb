7923d7be233ce9cceb2a69d367bed1958ea22356
dariodaprile <dario.daprile@gmail.com>
2013-05-22 19:57:33 +0100

    add exercise 33

--- test2.rb
+++ test2.rb
@@ -1,4 +1,33 @@
-for x in [1, 2, 3]
-  puts "I got #{x}"
+numbers =[]
+i = 0
+
+
+
+while i < 6
+  puts " At the top is #{i}"
+  i += 1
 end
+
+
+# [1, 2, 3, 4, 5].each { |i|
+#   puts "At the top i is #{i}"
+#   numbers.push(i)
+# }
+
+
+puts " numbers #{numbers}"
+
+
+# [1, 2, 3, 4, 5].each do |i|
+#   puts "At the top i is #{i}"
+# end
+
+# # while loop printing  6
+
+
+# for i in (0..5)
+#   puts "At the top i is #{i}"
+#   puts "Number is now #{i}"
+#   puts "At the botton is #{i+1}"
+# end
 
