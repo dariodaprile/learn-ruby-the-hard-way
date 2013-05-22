ea6dfb6ab98b7a7a8206570ec5b44eb724e573d3
dariodaprile <dario.daprile@gmail.com>
2013-05-22 20:14:25 +0100

    add exercise 34

---
+++ ex34.rb
@@ -0,0 +1,5 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "#{animals[0]} this is a bear"
+puts "#{animals.first} this is a bear"
+puts "#{animals.index('bear')} this is the position of the bear"
