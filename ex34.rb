d52a1890d8619a88cfe6f81b10cfca70cbf2c012
dariodaprile <dario.daprile@gmail.com>
2013-05-22 22:37:11 +0100

    add 35th exercise

--- ex34.rb
+++ ex34.rb
@@ -2,4 +2,9 @@ animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 
 puts "#{animals[0]} this is a bear"
 puts "#{animals.first} this is a bear"
+puts "#{animals.last} this is a latypus"
 puts "#{animals.index('bear')} this is the position of the bear"
+
+a = animals.index('peacock')
+b = animals[2]
+puts "this is the index #{a} of #{b}"
