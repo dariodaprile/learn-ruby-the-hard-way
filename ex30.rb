414611eaf3d48635dc03ae7e0b8d2db59d713020
dariodaprile <dario.daprile@gmail.com>
2013-05-22 18:11:37 +0100

    add exercise 30

---
+++ ex30.rb
@@ -0,0 +1,27 @@
+people = 30
+cars = 40
+buses =15
+
+if cars > people
+  puts "we should take cars"
+elsif cars < people
+  puts "we should not take the car"
+else
+  puts "we can't decide"
+end
+
+if buses > cars
+  puts "that's too many buses"
+elsif buses< cars
+  puts "maybe we could tal ethe buses"
+else
+  puts "we still can't decide"
+end
+
+if people > buses
+puts "Alright, let's just take the buses"
+else
+  puts "Fine let's stay home them"
+end
+
+
--- ex_anagram2.rb
+++ ex_anagram2.rb
@@ -3,7 +3,7 @@ input = ["stars", "mary", "rats", "tars", "army", "banana"]
 a = input.map {|word|
   letters = word.split("")
   permutations_as_letters = letters.permutation
-  permutations = permutations_as_letters.map(&:join)
+  permutations = permutations_as_letters.map(&:join) # &:join == {|letters| letters.join}
   anagrams = permutations & input
   anagrams.sort
 }
